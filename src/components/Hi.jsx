2e030c33bfb64cf77ae1a8f656e3590e8e59b961
Arijit Dey SarKar <129182340+Arijitdeysarkar@users.noreply.github.com>
2024-02-26 19:30:05 +0530

    first commit

---
+++ src/components/Hi.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import cheems from '../assets/download.jpg'
+
+export default function Hi() {
+  return (
+    <div>Hi I am Cheems
+      <img src={cheems} alt="profile" />
+    </div>
+ 
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Hi from './components/Hi'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Hi />
   </React.StrictMode>,
 )
